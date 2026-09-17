3147c10264f39fd23d3ce971c4336ededc3c6105
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-09-18 00:18:51 +0530

    model validator

--- learnPydantic/foundation/examples/model_behaviour.py
+++ learnPydantic/foundation/examples/model_behaviour.py
@@ -8,3 +8,33 @@ class User(BaseModel):
         if value < 4:
             raise ValueError("User name lenght must be more that 4 character")
         return value
+
+class SignupData(BaseModel):
+    password: str
+    confirm_password: str
+
+    @model_validator(mode='after')
+    def password_match(cls, values):
+        if values.password != values.confirm_password:
+            raise ValueError('Password don not match')
+        return values
+
+class Product(BaseModel):
+    price: float
+    quantity: int
+
+    @computed_field
+    @property
+    def total_price(self) -> float:
+        return self.price * self.quantity
+
+product = Product(price=100, quantity=3)
+
+print(product.price)
+# 100
+
+print(product.quantity)
+# 3
+
+print(product.total_price) # we don't call a method here because fo property decorator it acts like a value
+# 300
