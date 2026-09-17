33f062127289c72c3584b56f5e036a0c22513949
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-09-17 23:37:03 +0530

    basics

---
+++ learnPydantic/foundation/examples/fields_example.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional, List, Dict
+
+class Card(BaseModel):
+    user_id: int
+    items: List[str]
+    quantities: Dict[str, int]
+
+class BlogPost(BaseModel):
+    title: str
+    content: str
+    image_url: Optional[str] = None
---
+++ learnPydantic/foundation/solutions/employee_model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+# Field(...) -> denotes required field
+
+class Employee(BaseModel):
+    id: int
+    name: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description="Employee Name",
+        examples="John Doe"
+    )
+    department: Optional[str] = 'General'
+    salary: float = Field(..., ge=10000)
